9e2423db5167cb1c27ec92165352318aacbc51bd
Isabella Tea <tea.isabella@gmail.com>
2017-05-20 13:44:07 -0700

    completed requirements. all tests passing.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,52 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var objType = typeof obj;
+
+  //catchall
+  if (obj === null) {
+    return 'null';
+  }
+
+  if (objType === undefined) {
+    return;
+  }
+
+  //strings
+  if (objType === 'string') {
+    return '"' + obj + '"';
+  }
+
+  //numbers & booleans
+  if (objType === 'number' || objType === 'boolean') {
+    return obj.toString();
+  }
+
+  //arrays
+  if (Array.isArray(obj)) {
+    var stringedArray = [];
+    for (var i = 0; i < obj.length; i++) {
+      stringedArray.push(stringifyJSON(obj[i]));
+    }
+    return '[' + stringedArray + ']';
+  }
+
+  //objects
+  if (typeof obj === 'object') {
+
+    var keys = Object.keys(obj);
+    var stringifiedObject = '{';
+    var counter = 0;
+
+    for (var key in obj) {
+      if (stringifyJSON(obj[key])) {
+        stringifiedObject = stringifiedObject + stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + (counter < keys.length - 1 ? ',' : '');
+        counter++;
+      }
+    }
+
+    return stringifiedObject + '}';
+  }
+
+
 };
