000b748b6c999561b930d321e4b6f8daa964974b
Isabella Tea <tea.isabella@gmail.com>
2017-05-20 14:22:45 -0700

    completed requirements. all tests passing.

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,47 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+//Solution #1
+var getElementsByClassName = function(className) {
+  var results = [];
+  var allElements = document.body;
+
+  var checkForMatches = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      results.push(node);
+    }
+    for (var i = 0; i < node.childNodes.length; i++) {
+      checkForMatches (node.childNodes[i]);
+    }
+  };
+
+  checkForMatches (allElements);
+  return results;
+};
+
+
+//Solution #2
+var getElementsByClassName = function(className) {
+  var results = [];
+  var allElements = document.body;
+
+  var checkForMatches = function(node) {
+    if (node.classList && node.classList.contains(className)) {
+      results.push(node);
+    }
+  };
+
+  var traverse = function(node, callback) {
+    callback(node);
+    node = node.firstChild;
+    while (node) {
+      traverse (node, callback);
+      node = node.nextSibling;
+    }
+  };
+
+  traverse (allElements, checkForMatches);
+  return results;
+
 };
